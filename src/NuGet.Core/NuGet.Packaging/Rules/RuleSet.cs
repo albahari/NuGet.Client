a267d6dc8d36b2f282c98482f5c076de6c182984
Fernando Aguilar <feaguila@microsoft.com>
2019-06-18 17:42:54 -0700

    Icon size an open validation rulest. Draft

--- src/NuGet.Core/NuGet.Packaging/Rules/RuleSet.cs
+++ src/NuGet.Core/NuGet.Packaging/Rules/RuleSet.cs
@@ -27,6 +27,8 @@ namespace NuGet.Packaging.Rules
                 new PathTooLongRule(AnalysisResources.FilePathTooLongWarning),
                 new UnrecognizedLicenseIdentifierRule(AnalysisResources.UnrecognizedLicenseIdentifier),
                 new LicenseUrlDeprecationWarning(AnalysisResources.LicenseUrlDeprecationWarning),
+                new IconCanOpenRule(AnalysisResources.IconCannotOpenFile),
+                new IconMaxFilesizeRule(AnalysisResources.IconMaxFilsesizeExceeded),
             }
         );
 
